7f98048c7fcdec961682f49d7b329efe39bd1df2
kotpsot <zygmunt.justyna@gmail.com>
2018-03-29 12:20:48 +0100

    Removed Jasmine - will try mocha.

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 var http = require('http');
 
-http.createServer(function (req, res) {
+function App() {
+}
+
+
+this.server = http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello World!');
 }).listen(4000);
+
+
+exports.listen = function () {
+  this.server.listen.apply(this.server, arguments);
+};
+
+exports.close = function (callback) {
+  this.server.close(callback);
+};
